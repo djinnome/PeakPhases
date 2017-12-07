321df3bd26e28f6dffa7632f2d9b0a1b6f2ab3cf
Jeremy Zucker <Jeremy.Zucker@pnnl.gov>
2017-12-07 00:13:37 -0800

    Glycolysis PPP and TCA

--- Glycolysis-PPP/make_img_name.bash
+++ Glycolysis-PPP/make_img_name.bash
@@ -2,7 +2,7 @@
 SAVEIFS=$IFS
 IFS=$(echo -en "\n\b")
 i=0
-NAME=Glycolysis-TCA-Circadian
+NAME=Glycolysis-PPP
 for f in `ls -tr *.png`
 do
     cp $f $NAME-`printf "%02g" $i`.png
@@ -10,5 +10,5 @@ do
 done
 #RNA=`ls -1 NAME-*.png`
 #convert -delay "100" -quality 100 -verbose $RNA RNAOscillations150.mpg
-ffmpeg -f image2 -r 1 -i %02d-$NAME.png -vcodec mpeg4 -y $NAME-Circadian.mp4
+ffmpeg -f image2 -r 1 -i $NAME-%02d.png -vcodec mpeg4 -y $NAME.mp4
 IFS=$SAVEIFS
